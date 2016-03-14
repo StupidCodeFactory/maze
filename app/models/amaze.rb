bca41573e9b1b90c5c37def3c32b47055ebdbeb4
Yann Marquet <ymarquet@gmail.com>
2016-03-14 01:18:38 +0000

    implement the backtracking algorythm

--- app/models/amaze.rb
+++ app/models/amaze.rb
@@ -1,21 +1,49 @@
+require 'pp'
+
 class Amaze
 
+  attr_accessor :start_x, :start_y, :end_x, :end_y
+
   def initialize(width, height)
-    self.width  = width
-    self.height = height
+    self.width                = width
+    self.height               = height
+    self.start_x              = rand(width)
+    self.start_y              = 0
+    self.grid                 = Array.new(height) { |y| Array.new(width) { |x| Cell.new(x,y, self)} }
+    self.visisted_cells_stack = []
+    self.end_x                = rand(width)
+    self.end_y                = height - 1
+  end
 
-    self.start_x = rand(width)
-    self.start_y = 0
+  def rows
+    grid
+  end
 
-    self.grid  = Array.new(width) { |y| Array.new(height) { |x| Cell.new(x,y)} }
+  def cell_at(x, y)
+    return if [x, y].min < 0
+    grid[y] && grid[y][x]
   end
 
   def generate
-    visit_cell(start_x, start_y)
+    visit_cells(grid[start_y][start_x])
   end
 
   private
 
-  attr_accessor :width, :height, :start_x, :start_y, :grid
+  attr_accessor :width, :height, :grid, :visisted_cells_stack
 
+  def visit_cells(cell)
+
+    if direction = cell.unvisited_random_direction
+      new_cell = cell.connect_to(direction)
+      new_cell.visit!
+      visisted_cells_stack.push(new_cell)
+      visit_cells(new_cell)
+    else
+      # self.end_x, self.end_y = cell.to_a
+      new_cell = visisted_cells_stack.pop
+      visit_cells(new_cell) if new_cell
+    end
+
+  end
 end
--- spec/models/amaze_spec.rb
+++ spec/models/amaze_spec.rb
@@ -1,19 +1,31 @@
 require "rails_helper"
 
 RSpec.describe Amaze do
-  let(:width)  { 12 }
-  let(:height) { 10 }
+  let(:width)  { 4 }
+  let(:height) { 8 }
   subject { described_class.new(width, height) }
 
   it 'takes a width and heigth' do
     expect { subject }.to_not raise_error
   end
 
+  describe '#cell_at' do
+    it 'finds the cell at (x, y) coordinates' do
+      expect(subject.cell_at(1,2).to_a).to eq([1,2])
+    end
+
+    context 'with out of bounds coordonates' do
+      it 'returns nil' do
+        expect(subject.cell_at(2))
+      end
+    end
+  end
+
   describe 'when initialized' do
 
     describe '#generate' do
       it 'generate maze' do
-        pp subject.generate
+        subject.generate
       end
     end
 
