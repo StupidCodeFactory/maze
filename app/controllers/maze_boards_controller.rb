44ea9666af75e982f5ae9cf4612bfb1577689bc8
Yann Marquet <ymarquet@gmail.com>
2016-03-14 02:53:24 +0000

    maze create action

--- app/controllers/maze_boards_controller.rb
+++ app/controllers/maze_boards_controller.rb
@@ -1,8 +1,26 @@
 class MazeBoardsController < ApplicationController
+
+  def new
+    @maze_form = MazeForm.new(height: 6, width: 8)
+  end
+
   def show
     height = width = 16
     @maze = Amaze.new(width, height)
     @maze.generate
   end
 
+  def create
+    @maze_form = MazeForm.new(maze_form_params)
+    if @maze_form.valid?
+      maze = @maze_form.save
+      redirect_to maze
+    end
+  end
+
+  private
+
+  def maze_form_params
+    params.require(:maze_form).permit(:height, :width)
+  end
 end
